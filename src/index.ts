9fb01087a2e490a554c1c5891431eaba4affa02b
Kayo Ronald <kayoronald4321@gmail.com>
2021-07-17 17:57:34 +0000

    update file

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,11 @@
 import express from 'express';
 import cors from 'cors'
 import './database/connection'
+import routes from './routes/routes';
 
 const app = express();
 app.use(express.json());
 
 app.use(cors())
-
+app.use(routes);
 app.listen(8080);
