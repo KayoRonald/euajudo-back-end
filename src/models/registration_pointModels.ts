5b077e16e49e5323d45515acd41d287f6c048737
Kayo Ronald <kayoronald4321@gmail.com>
2021-07-17 18:13:17 +0000

    Create Model

---
+++ src/models/registration_pointModels.ts
@@ -0,0 +1,29 @@
+import { Entity, Column, PrimaryGeneratedColumn } from 'typeorm'
+
+@Entity()
+export default class RegistionPoint {
+
+  @PrimaryGeneratedColumn('increment')
+  id: number;
+
+  @Column('')
+  namePoint: string;
+
+  @Column('')
+  latitude: string;
+
+  @Column('')
+  longitude: number;
+
+  @Column('')
+  about: string;
+
+  @Column('')
+  whatsapp: number;
+
+  @Column('')
+  responsible_name: string;
+
+  @Column('')
+  type: string;
+}
