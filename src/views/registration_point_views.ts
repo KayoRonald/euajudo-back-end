001068cfd8bd6ab84da8e0bbb93406ea6bbf4075
Diqxy1 <kusanagitakeru@outlook.com>
2021-07-23 20:12:15 -0300

    variable renamed, eslint changed the rest

--- src/views/registration_point_views.ts
+++ src/views/registration_point_views.ts
@@ -1,16 +1,17 @@
-import RegistionPoint from '../models/registration_pointModels'
+import RegistionPoint from '../modules/Registration/typeorm/models/registration_pointModels';
 
-export default{
+export default {
+  // eslint-disable-next-line
   render(registionPoint: RegistionPoint) {
-    return{
+    return {
       id: registionPoint.id,
       namePoint: registionPoint.namePoint,
       latitude: registionPoint.latitude,
       longitude: registionPoint.longitude,
       about: registionPoint.about,
       whatsapp: registionPoint.whatsapp,
-      responsible_name: registionPoint.responsible_name,
-      typePoint: registionPoint.typePoint
-    }
-  }
-}
+      responsible_name: registionPoint.responsibleName,
+      typePoint: registionPoint.typePoint,
+    };
+  },
+};
