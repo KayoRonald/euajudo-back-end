6371725a126fc909caa32229a1f917d6387a12b3
Diqxy1 <kusanagitakeru@outlook.com>
2021-07-23 20:08:27 -0300

    imports in correct order and more readable code

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,18 @@
+// External Imports
 import express from 'express';
-import cors from 'cors'
-import './database/connection'
+import cors from 'cors';
+
+// Internal Imports
+import './database/connection';
 import routes from './routes/routes';
 
 const app = express();
+
+app.use(cors());
 app.use(express.json());
 
-app.use(cors())
 app.use(routes);
-app.listen(8080);
+
+app.listen(8080, () => {
+  console.log('🔥 Server started http://localhost:8080');
+});
