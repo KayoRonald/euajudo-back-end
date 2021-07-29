8ffe4c9e1b34148adcb4bdd0963246da5669e961
Diqxy1 <kusanagitakeru@outlook.com>
2021-07-29 20:51:00 -0300

    refactor: adicionado serviços de update e delete ao controller

--- src/modules/Registration/controllers/RegistrationPointControllers.ts
+++ src/modules/Registration/controllers/RegistrationPointControllers.ts
@@ -1,7 +1,9 @@
 import { Request, Response } from 'express';
+import DeleteRegistrationPoinService from '../services/DeleteService';
 
 import ListService from '../services/ListService';
 import RegistrationPointService from '../services/RegistrationService';
+import UpdateServie from '../services/UpdateService';
 
 export default class RegistionPointController {
   async create(request: Request, response: Response): Promise<Response> {
@@ -36,5 +38,48 @@ export default class RegistionPointController {
     const list = await listPoint.execute();
 
     return response.status(200).json(list);
+  }
+
+  async delete(request: Request, response: Response): Promise<Response> {
+    const { id } = request.params;
+
+    const deletePoint = new DeleteRegistrationPoinService();
+
+    const pointId = parseInt(id);
+
+    await deletePoint.execute({ pointId });
+
+    return response.status(200).json('Ponto deleteado com sucesso');
+  }
+
+  async update(request: Request, response: Response): Promise<Response> {
+    const { id } = request.params;
+
+    const {
+      about,
+      latitude,
+      longitude,
+      namePoint,
+      responsibleName,
+      typePoint,
+      whatsapp,
+    } = request.body;
+
+    const updatePoint = new UpdateServie();
+
+    const pointId = parseInt(id);
+
+    const update = await updatePoint.execute({
+      id: pointId,
+      about,
+      latitude,
+      longitude,
+      namePoint,
+      responsibleName,
+      typePoint,
+      whatsapp,
+    });
+
+    return response.status(200).json(update);
   }
 }
