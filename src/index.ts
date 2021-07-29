3152875da44d9b65603b1bfebab5cc60da3d5cc2
Diqxy1 <kusanagitakeru@outlook.com>
2021-07-29 20:53:32 -0300

    refactor: adicionado tratamento de erro customizado no server

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,12 @@
 // External Imports
-import express from 'express';
+import express, { NextFunction, Request, Response } from 'express';
 import cors from 'cors';
+import 'express-async-errors';
 
 // Internal Imports
 import './database/connection';
 import routes from './routes/routes';
+import AppError from './errors/AppError';
 
 const app = express();
 
@@ -12,6 +14,22 @@ app.use(cors());
 app.use(express.json());
 
 app.use(routes);
+
+app.use(
+  (error: Error, request: Request, response: Response, _: NextFunction) => {
+    if (error instanceof AppError) {
+      return response.status(error.statusCode).json({
+        status: 'error',
+        message: error.message,
+      });
+    }
+
+    return response.status(500).json({
+      status: 'error',
+      message: 'Internal server error',
+    });
+  },
+);
 
 app.listen(8080, () => {
   console.log('🔥 Server started http://localhost:8080');
