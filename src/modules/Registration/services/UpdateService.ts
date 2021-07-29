53340d403037e58e6619e36300e8f818cd1ede1f
Diqxy1 <kusanagitakeru@outlook.com>
2021-07-29 20:49:52 -0300

    feat: adicionado serviço de atualização

---
+++ src/modules/Registration/services/UpdateService.ts
@@ -0,0 +1,48 @@
+import { getCustomRepository } from 'typeorm';
+import AppError from '../../../errors/AppError';
+import RegistionPoint from '../typeorm/models/registration_pointModels';
+import { RegistionPointRepository } from '../typeorm/repositories/RegistrationRepository';
+
+interface IRequest {
+  id: number;
+  namePoint: string;
+  latitude: number;
+  longitude: number;
+  about: string;
+  whatsapp: number;
+  responsibleName: string;
+  typePoint: string;
+}
+
+export default class UpdateServie {
+  public async execute({
+    id,
+    about,
+    latitude,
+    longitude,
+    namePoint,
+    responsibleName,
+    typePoint,
+    whatsapp,
+  }: IRequest): Promise<RegistionPoint> {
+    const registrationRepository = getCustomRepository(
+      RegistionPointRepository,
+    );
+
+    const point = await registrationRepository.findOne({ where: { id: id } });
+
+    if (!point) {
+      throw new AppError('Ponto de registro não encontrado');
+    }
+
+    point.about = about;
+    point.latitude = latitude;
+    point.longitude = longitude;
+    point.namePoint = namePoint;
+    point.responsibleName = responsibleName;
+    point.typePoint = typePoint;
+    point.whatsapp = whatsapp;
+
+    return await registrationRepository.save(point);
+  }
+}
