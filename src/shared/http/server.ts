185c7604ceafc0b7c4d6f02d7d86e71ab54e2d4d
Diqxy1 <kusanagitakeru@outlook.com>
2021-08-01 07:24:42 -0300

    refactor: change the server location to a more scalable and readable structure, and added imports needed to upload the api

---
+++ src/shared/http/server.ts
@@ -0,0 +1,43 @@
+// External Imports
+import 'reflect-metadata';
+import 'dotenv/config';
+import express, { NextFunction, Request, Response } from 'express';
+import cors from 'cors';
+import 'express-async-errors';
+
+// Internal Imports
+import routes from './routes/routes';
+import AppError from '@shared/errors/AppError';
+import '@shared/typeorm';
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+app.use(routes);
+
+app.use(
+  (error: Error, request: Request, response: Response, _: NextFunction) => {
+    if (error instanceof AppError) {
+      return response.status(error.statusCode).json({
+        status: 'error',
+        message: error.message,
+      });
+    }
+
+    console.error(error);
+
+    return response.status(500).json({
+      status: 'error',
+      message: 'Internal server error',
+    });
+  },
+);
+
+const port = process.env.PORT || 8080;
+
+app.listen(port, () => {
+  // eslint-disable-next-line
+  console.log('🔥 Server started http://localhost:8080');
+});
