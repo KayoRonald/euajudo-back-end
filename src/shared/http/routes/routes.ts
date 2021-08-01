38076d0efd6966b8598aa61a8a9124e01b829121
Diqxy1 <kusanagitakeru@outlook.com>
2021-08-01 07:22:44 -0300

    refactor: change routes location to a more scalable and readable structure

---
+++ src/shared/http/routes/routes.ts
@@ -0,0 +1,9 @@
+import { Router } from 'express';
+
+import registionRoutes from '@modules/Registration/routes/Register.routes';
+
+const routes = Router();
+
+routes.use('/registionPoint', registionRoutes);
+
+export default routes;
