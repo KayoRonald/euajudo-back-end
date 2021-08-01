c6ac645baeb17113780d45a172e7a72a28f7b0a7
Diqxy1 <kusanagitakeru@outlook.com>
2021-08-01 07:27:05 -0300

    remove: deleted unnecessary files

--- src/routes/routes.ts
+++
@@ -1,9 +0,0 @@
-import { Router } from 'express';
-
-import registionRoutes from '../modules/Registration/routes/Register.routes';
-
-const routes = Router();
-
-routes.use('/registionPoint', registionRoutes);
-
-export default routes;
